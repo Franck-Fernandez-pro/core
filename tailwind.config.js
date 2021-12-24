3881d0626b951db902aeb554db6e06674e7f31b7
Franck Fernandez <franck.fernandez@gmail.com>
2021-12-24 10:32:49 +0100

    Tailwind course

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#0FA9E6',
+          light: '#3FBAEB',
+          dark: '#0C87B8',
+        },
+      },
+      fontFamily: {
+        headline: 'Poppins, sans-sherif',
+      },
+    },
   },
   plugins: [],
-}
+};
